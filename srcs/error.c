a9fcf4993131c0e7abdd55dced074da91f2b09b5
Thiwanka Somachandra <thivankas@outlook.com>
2025-04-23 13:51:29 +0300

    Re-arrange the code to comply norm stds

--- srcs/error.c
+++ srcs/error.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 13:50:44 by tsomacha          #+#    #+#             */
+/*   Updated: 2025/04/23 13:50:49 by tsomacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/shell.h"
 
-int syntax_error(char *input, char *msg, int code)
+int	syntax_error(char *input, char *msg, int code)
 {
 	free(input);
 	ft_putendl_fd(msg, 2);
