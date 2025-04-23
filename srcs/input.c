2be9d0ff75e274c41345e98df1058f4ee3ba3ad1
Thiwanka Somachandra <thivankas@outlook.com>
2025-04-23 15:41:36 +0300

    Re-arranging the code to comply with norminette stds

--- srcs/input.c
+++ srcs/input.c
@@ -1,18 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 15:38:37 by tsomacha          #+#    #+#             */
+/*   Updated: 2025/04/23 15:41:04 by tsomacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/shell.h"
 
-t_cmd *handel_input(t_shell *mini, char *token)
+t_cmd	*handel_input(t_shell *mini, char *token)
 {
-	/**
-	 * example of the command format
-	 * cat < file.txt
-	*/
-	t_cmd *cmd = malloc(sizeof(t_cmd));
-	if(!cmd)
+	char	*str;
+	char	*arg_str;
+	t_cmd	*cmd;
+
+	cmd = malloc(sizeof(t_cmd));
+	if (!cmd)
 		return (NULL);
-	char *str = ft_strchr(token, '<');
-	if(str && str[1] && str[1] == '<')
-		return(handle_heredoc(mini, cmd, token));
-	char *arg_str = set_arg_string(token, '<');
+	str = ft_strchr(token, '<');
+	if (str && str[1] && str[1] == '<')
+		return (handle_heredoc(mini, cmd, token));
+	arg_str = set_arg_string(token, '<');
 	cmd->type = set_command_type(token);
 	cmd->command = set_path_name(mini, arg_str);
 	cmd->filename = set_filename(token, '<');
